86a2276d9fd70a63aa220209e98a4c802da50bd0
Karman Karman <karman131313@gmail.com>
2024-03-08 13:41:16 +0100

    adding english version to CV page

---
+++ src/icons/ArrowRight.ts
@@ -0,0 +1,18 @@
+export const ArrowRight = `
+<svg
+  style="margin-right:8px"
+  xmlns="http://www.w3.org/2000/svg"
+  width="20"
+  height="20"
+  viewBox="0 0 24 24"
+  stroke-width="1.5"
+  stroke="currentColor"
+  fill="none"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+>
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+  <path d="M5 12l14 0"></path>
+  <path d="M15 16l4 -4"></path>
+  <path d="M15 8l4 4"></path>
+</svg>`
